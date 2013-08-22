c2bdee025f64d39cf28dcb81d9277569c40f3d0d
Jamie from Desktop <jamiebclark@gmail.com>
2013-08-22 10:06:07 -0400

    Added Embed Fit to javascript

--- webroot/js/script.js
+++ webroot/js/script.js
@@ -578,3 +578,33 @@ documentReady(function() {
 documentReady(function() {
 	$('.media').layoutMedia();
 });
+
+// Embed Fit
+(function($) {
+	$.fn.embedFit = function() {
+		return this.each(function() {
+			var $container = $(this),
+				$embedObject = $('object,iframe', $container).first(),
+				$embedObjects = $('embed,object,iframe', $container),
+				embedWidth = $embedObject.attr('width') ? $embedObject.attr('width') : $embedObject.width(),
+				embedHeight = $embedObject.attr('height') ? $embedObject.attr('height') : $embedObject.height(),
+				embedRatio = embedWidth / embedHeight;
+			
+			console.log('Fitting object');
+			function fitEmbedObject() {
+				var w = $container.width();
+				$embedObjects.width(w).height(w / embedRatio);
+			}
+			if (!$container.data('embed-fit-init')) {
+				fitEmbedObject();
+				$(window).resize(function() {
+					fitEmbedObject();
+				});	
+				$container.data('embed-fit-init', true);
+			}
+		});
+	};
+})(jQuery);
+documentReady(function() {
+	$('.embed-fit').embedFit();
+});
