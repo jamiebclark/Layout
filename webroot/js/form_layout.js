6b4b83ecd5899abd0cb9f7369d88d2e83be9ebe2
Jamie from Desktop <jamiebclark@gmail.com>
2013-09-29 12:54:48 -0400

    Added textarea autoresize function for form_layout.js

--- webroot/js/form_layout.js
+++ webroot/js/form_layout.js
@@ -1526,3 +1526,35 @@ documentReady(function() {
 documentReady(function() {
 	$('.input-centerfocus').inputCenterFocus();
 });
+
+(function($) {
+	$.fn.textareaAutoresize = function() {
+		return this.each(function() {
+			var $textarea = $(this),
+				h = 0,
+				textarea = this;
+			function resize() {
+				//textarea.style.height = '0px';
+				var newHeight = textarea.scrollHeight;
+				if (newHeight != h) {
+					$textarea.stop().animate({'height': (newHeight) + 'px'});
+					h = newHeight;
+				}
+//				textarea.style.height = (textarea.scrollHeight + 10) + 'px';
+			}
+			if (!$textarea.data('textareaAutoresize-init')) {
+				$textarea
+					.css({'overflow': 'hidden', 'height': '0px'})
+					.keyup(function() {
+						resize();
+					})
+					.data('textareaAutoresize-init', true);
+			}
+			resize();
+			return $textarea;
+		});
+	};
+})(jQuery);
+documentReady(function() {
+	$('textarea.textarea-autoresize').textareaAutoresize();
+});
