09b0a24f8eff87b30257c13fe1fcf2f5da61ce34
Jamie from Desktop <jamiebclark@gmail.com>
2013-11-24 13:31:26 -0500

    Updated form-layout input-centerfocus

--- webroot/js/form_layout.js
+++ webroot/js/form_layout.js
@@ -1464,9 +1464,16 @@ documentReady(function() {
 	$.fn.inputCenterFocus = function() {
 		return this.each(function() {
 			var $input = $(this),
+				$label = $('<div></div>', {
+					'class': 'input-centerfocus-label',
+					'html': 'Click outside of the textbox to return to normal view'
+				}),
+				$placeholder = $('<div class="input-centerfocus-placeholder"></div>'),
+				$bg = $('<div class="input-centerfocus-bg"></div>'),
+				$focusElements = $([$label[0], $placeholder[0], $bg[0]]),
 				inputOffset,
 				oWidth = $input.css('width'),
-				oHeight = $input.css('height'),
+				oHeight = $input.outerHeight(),
 				oZIndex = $input.css('z-index'),
 				oPos = $input.css('position'),
 				screenW,
@@ -1478,9 +1485,14 @@ documentReady(function() {
 				boxEndWidth,
 				boxEndHeight,
 				boxPctWidth = .8,
-				boxPctHeight = .8;
-			
+				boxPctHeight = .8,
+				isWindowFocused = true,
+				isFocused = false;
+				
 			function grow() {
+				if (!isWindowFocused) {
+					return;
+				}
 				inputOffset = $input.offset();
 				boxStartTop = inputOffset.top;
 				boxStartLeft = inputOffset.left;
@@ -1493,40 +1505,75 @@ documentReady(function() {
 				boxEndTop = (screenH - boxEndHeight) / 2;
 				boxEndLeft = (screenW - boxEndWidth) / 2;
 				
+				var boxTop = (boxStartTop - $(window).scrollTop()),
+					boxLeft = (boxStartLeft - $(window).scrollLeft());
+				$placeholder.css({
+					'width': oWidth,
+					'height': oHeight
+				});
 				$(this)
+					.addClass('focused')
 					.css({
-						'position': 'fixed', 
-						'z-index': 1000,
 						'width': oWidth,
 						'height': oHeight,
-						'left': boxStartLeft,
-						'top': boxStartTop
+						'left': boxLeft,
+						'top': boxTop
 					})
 					.animate({
 						'width': boxEndWidth,
 						'height': boxEndHeight,
 						'left': boxEndLeft,
 						'top': boxEndTop
+					}, {
+						'complete': function() {
+							$label.css({
+								'top': boxEndTop + boxEndHeight,
+								'left': boxEndLeft,
+								'width': boxEndWidth
+							});
+							$focusElements.each(function() {
+								$(this).addClass('focused').fadeIn();
+							});	
+						}
 					});
+					isFocused = true;
 			}
 			
 			function shrink() {
-				$(this).animate({
-					'width': oWidth,
-					'height': oHeight,
-					'left': boxStartLeft,
-					'top': boxStartTop
-				}, {
-					complete: function() {
-						$(this).css({
-							'position': oPos,
-							'z-index': oZIndex
-						});
-					}
-				})
+				if (!isWindowFocused) {
+					return;
+				}
+				$focusElements.each(function() {
+					$(this).removeClass('focused').hide();
+				});
+				$(this)
+					.animate({
+						'width': oWidth,
+						'height': oHeight,
+						'left': (boxStartLeft - $(window).scrollLeft()),
+						'top': (boxStartTop - $(window).scrollTop())
+					}, {
+						complete: function() {
+							$(this).removeClass('focused');
+							isFocused = false;
+						}
+					});
 			}
 			
 			if (!$input.data('centerfocus-init')) {
+				$focusElements.each(function() {
+					$(this).hide().insertAfter($input);
+				});
+				$(window).focus(function() {
+					isWindowFocused = true;
+					if (isFocused) {
+						grow();
+					} else {
+						shrink();
+					}
+				}).blur(function() {
+					isWindowFocused = false;
+				});
 				$input.focus(grow).blur(shrink).data('centerfocus-init', true);
 			}				
 		});
