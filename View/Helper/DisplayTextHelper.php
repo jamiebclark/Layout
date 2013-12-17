7be389363e579e7b6068edec010e7d1114055044
Jamie from Desktop <jamiebclark@gmail.com>
2013-12-17 14:35:20 -0500

    Fixed minor DisplayText formatting

--- View/Helper/DisplayTextHelper.php
+++ View/Helper/DisplayTextHelper.php
@@ -217,8 +217,8 @@ class DisplayTextHelper extends LayoutAppHelper {
 			'/["]{2}(.*?)["]{2}/s'											=>  "<blockquote><div>$1</div></blockquote>",
 			
 			//Updates List Items
-			'/\n-[\s]+(.*?)[\r]*[\n]*[\r]*[\n]*\r/sm'			=>	'<uli>$1</uli>',
-			'/\n[\d]+\.[\s]+([^\r\n]+)[\r]*/sm'					=>	'<oli>$1</oli>',
+			'/[\r\n]+-[\s]+(.*?)[\r]*[\n]*[\r]*[\n]*$/sm'		=>	'<uli>$1</uli>',
+			'/[\r\n]+[\d]+\.[\s]+([^\r\n]+)[\r]*$/sm'			=>	'<oli>$1</oli>',
 			'#((?<!uli\>)<uli>.*?</uli>(?!\<uli))#m'			=>	'<ul>$1</ul>',
 			'#((?<!oli\>)<oli>.*?</oli>(?!\<oli))#m'			=>	'<ol>$1</ol>',
 			'/<[o|u]li>/'										=>	'<li>',
@@ -319,6 +319,7 @@ class DisplayTextHelper extends LayoutAppHelper {
 	}
 	
 	function pregReplaceArray($regx, $str) {
+		$str = "\n$str";
 		$str = preg_replace(array_keys($regx),$regx,$str,-1,$count);
 		return $str;
 	}
