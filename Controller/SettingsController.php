466b7671f4fff972867c2f99a37bb7ceaae36ff5
Jamie from Desktop <jamiebclark@gmail.com>
2014-02-26 11:47:49 -0500

    Added bootstrap 3 files

---
+++ Controller/SettingsController.php
@@ -0,0 +1,13 @@
+<?php
+class SettingsController extends LayoutAppController {
+	public $name = 'Settings';
+	public $uses = false;
+	
+	/**
+	 * Searches directories and converts camelCase variables to dashed variable
+	 *
+	 **/
+	public function admin_sass_convert() {
+		$directories = array();
+	}
+}
