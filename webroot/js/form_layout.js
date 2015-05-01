ab46962f4af1006da97a96230d7ef0a912cbf6fe
Jamie Clark Desktop <jamiebclark@gmail.com>
2015-05-01 11:03:10 -0400

    Added tab inputs to the script

--- webroot/js/form_layout.js
+++ webroot/js/form_layout.js
@@ -1896,3 +1896,24 @@ documentReady(function() {
 		return $(this);
 	});
 });
+
+// Allows the user to enter tabs into an inpu
+(function($) {
+	$(document).ready(function() {
+		var tabKeyCode = 9;
+		var tabInsert = "\t";
+		$('.js-input-tabbed').each(function() {
+			$(this).keydown(function(e) {
+				var $this, end, start;
+				if (e.keyCode == tabKeyCode) {
+					start = this.selectionStart;
+					end = this.selectionEnd;
+					$this = $(this);
+					$this.val($this.val().substring(0, start) + tabInsert + $this.val().substring(end));
+					this.selectionStart = this.selectionEnd = start + 1;
+					return false;
+				}
+			});
+		});
+	});
+})(jQuery);
