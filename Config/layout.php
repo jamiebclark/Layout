17210be750952dbbb283045b42a351b386504d28
Jamie Desktop <jamiebclark@gmail.com>
2017-07-10 09:53:28 -0400

    Added AssetLibrary config

---
+++ Config/layout.php
@@ -0,0 +1,15 @@
+<?php
+$config = [
+	'Layout' => [
+		'AssetLibrary' => [
+			'jquery' => [
+				'css' => [],
+				'js' => ['//ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js']
+			],
+			'jquery-ui' => [
+				'css' => ['//ajax.googleapis.com/ajax/libs/jqueryui/1.12.1/themes/smoothness/jquery-ui.css'],
+				'js' => ['//ajax.googleapis.com/ajax/libs/jqueryui/1.12.1/jquery-ui.min.js'],
+			]
+		],
+	]
+];
